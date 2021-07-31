ac53554941a6a51db5fed5232b1d9b37dcc69646
Ben Chavez <ben.m.chavez@gmail.com>
2021-07-31 11:01:41 -0500

    sidebar

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Navbar';
+import Sidebar from '../Sidebar';
 import {
   HeroContainer,
   HeroContent,
@@ -10,12 +11,19 @@ import {
 } from './HeroElements';
 
 const Hero = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <HeroContainer>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
       <HeroContent>
         <HeroItems>
-          <HeroH1>Greates Pizza Ever</HeroH1>
+          <HeroH1>Greatest Pizza Ever</HeroH1>
           <HeroP>Ready in 60 seconds</HeroP>
           <HeroBtn>Place Order!</HeroBtn>
         </HeroItems>
